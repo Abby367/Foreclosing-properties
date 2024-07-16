65711825003ebc875deb764d39b105f5bf03e01d
Abigail Anne Calaminos <57781516+Abby367@users.noreply.github.com>
2024-07-16 15:12:08 +0800

    front-end responsiveness

--- foreclosed-frontend/src/components/navigationbar/topnav.tsx
+++ foreclosed-frontend/src/components/navigationbar/topnav.tsx
@@ -1,81 +1,188 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/Logo.png";
-import { Box, Button, Link, Stack } from '@mui/joy';
+import { Box, Button, Link, Stack, IconButton, Drawer, List, ListItem } from '@mui/joy';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function TopNav() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const menuItems = (
+    <>
+      <Link 
+        href="#" 
+        fontWeight="bold" 
+        color="neutral" 
+        underline="none"
+        sx={{ 
+          '&:hover': { 
+            color: '#007FFF' 
+          }
+        }}
+      >
+        Home
+      </Link>
+      <Link 
+        href="#" 
+        fontWeight="bold" 
+        color="neutral" 
+        underline="none"
+        sx={{ 
+          '&:hover': { 
+            color: '#007FFF' 
+          }
+        }}
+      >
+        Properties for sale
+      </Link>
+      <Link 
+        href="#" 
+        fontWeight="bold" 
+        color="neutral" 
+        underline="none"
+        sx={{ 
+          '&:hover': { 
+            color: '#007FFF' 
+          }
+        }}
+      >
+        About Us
+      </Link>
+      <Link 
+        href="#" 
+        fontWeight="bold" 
+        color="neutral" 
+        underline="none"
+        sx={{ 
+          bgcolor: 'white', 
+          borderRadius: 1, 
+          p: 1,
+          '&:hover': { 
+            color: '#007FFF', 
+          }
+        }}
+      >
+        FAQs
+      </Link>
+      <Button 
+        variant="solid"
+        sx={{ 
+          bgcolor:'#007FFF',
+          width:'120px',
+          '&:hover': { 
+            bgcolor: 'primary.dark' 
+          } 
+        }}
+      >
+        Login
+      </Button>
+    </>
+  );
+
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 1, bgcolor: 'white',  width:'100vw'}}>
-      <img width={150} src={Logo} alt="Logo"/> 
+    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 1, bgcolor: 'white', width:{xs:'90%',md:'100%'}}}>
+      <img width={150} src={Logo} alt="Logo" /> 
     
-      <Stack direction="row" spacing={4} marginRight={"20px"}>
-        <Link 
-          href="#" 
-          fontWeight="bold" 
-          color="neutral" 
-          underline="none"
-          sx={{ 
-            '&:hover': { 
-              color: '#007FFF' 
-            }
-          }}
-        >
-          Home
-        </Link>
-        <Link 
-          href="#" 
-          fontWeight="bold" 
-          color="neutral" 
-          underline="none"
-          sx={{ 
-            '&:hover': { 
-              color: '#007FFF' 
-            }
-          }}
-        >
-          Properties for sale
-        </Link>
-        <Link 
-          href="#" 
-          fontWeight="bold" 
-          color="neutral" 
-          underline="none"
-          sx={{ 
-            '&:hover': { 
-              color: '#007FFF' 
-            }
-          }}
-        >
-          About Us
-        </Link>
-        <Link 
-          href="#" 
-          fontWeight="bold" 
-          color="neutral" 
-          underline="none"
-          sx={{ 
-            bgcolor: 'white', 
-            borderRadius: 1, 
-            p: 1,
-            '&:hover': { 
-              color: '#007FFF', 
-            }
-          }}
-        >
-          FAQs
-        </Link>
-        <Button 
-          variant="solid"
-          sx={{ 
-            bgcolor:'#007FFF',
-            width:'120px',
-            '&:hover': { 
-              bgcolor: 'primary.dark' 
-            } 
-          }}
-        >
-          Login
-        </Button>
-      </Stack>
+      <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+        <Stack direction="row" spacing={4} marginRight={"20px"}>
+          {menuItems}
+        </Stack>
+      </Box>
+
+      <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+        <IconButton onClick={handleDrawerToggle} sx={{  color: '#007FFF' }}>
+          <MenuIcon />
+        </IconButton>
+      </Box>
+
+      <Drawer
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+        sx={{ display: { xs: 'block', md: 'none' } }}
+      >
+        <List>
+          <ListItem>
+            <Link 
+              href="#" 
+              fontWeight="bold" 
+              color="neutral" 
+              underline="none"
+              sx={{ 
+                '&:hover': { 
+                  color: '#007FFF' 
+                }
+              }}
+            >
+              Home
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link 
+              href="#" 
+              fontWeight="bold" 
+              color="neutral" 
+              underline="none"
+              sx={{ 
+                '&:hover': { 
+                  color: '#007FFF' 
+                }
+              }}
+            >
+              Properties for sale
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link 
+              href="#" 
+              fontWeight="bold" 
+              color="neutral" 
+              underline="none"
+              sx={{ 
+                '&:hover': { 
+                  color: '#007FFF' 
+                }
+              }}
+            >
+              About Us
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link 
+              href="#" 
+              fontWeight="bold" 
+              color="neutral" 
+              underline="none"
+              sx={{ 
+                bgcolor: 'white', 
+                borderRadius: 1, 
+                p: 1,
+                '&:hover': { 
+                  color: '#007FFF', 
+                }
+              }}
+            >
+              FAQs
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Button 
+              variant="solid"
+              sx={{ 
+                bgcolor:'#007FFF',
+                width:'100%',
+                '&:hover': { 
+                  bgcolor: 'primary.dark' 
+                } 
+              }}
+            >
+              Login
+            </Button>
+          </ListItem>
+        </List>
+      </Drawer>
     </Box>
   );
 }
--- foreclosed-frontend/src/components/searchbar/Landingsearch.tsx
+++ foreclosed-frontend/src/components/searchbar/Landingsearch.tsx
@@ -1,35 +1,51 @@
 import React from "react";
 import Landingcarousel from "../carousel/Landingcarousel";
-import { Box, Typography, Button, Input } from '@mui/joy';
+import { Box, Typography, Input } from '@mui/joy';
+import { Search } from "@mui/icons-material";
 
 export default function Landingsearch() {
   return (
-    <Box sx={{ marginTop: 4, width: '95vw', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', columnGap:'20px' }}>
-      <Box sx={{ width: { xs: '100%', md: '40vw'}}}>
-        <Box sx={{width: { xs: '100%', md: '50%'}}}>
-        <Typography
-            level="h1"
+    <Box sx={{ marginTop: 4, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', columnGap: '20px' }}>
+      <Box sx={{ width: { xs: '100%', md: '40vw' }, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Box sx={{ width: { xs: '100%', md: '80%' } }}>
+          <Typography
+            fontSize={"32px"}
             fontWeight="bold"
             component="h1"
             sx={{
-                width: { xs: '100%', md: '293px'}
-             
+              width: { xs: '100%', md: '380px' }
             }}
           >
             The better way to buy <Typography component="span" sx={{ color: '#007FFF' }}>real estate</Typography>
           </Typography>
         </Box>
-        <Box sx={{ width: '50%' }}>
-          <Typography level="body-sm" fontWeight="500" component="p" sx={{ mt: 2 ,width: { xs: '100%', md: '293px'}}}>
+        <Box sx={{ width: { xs: '100%', md: '80%' } }}>
+          <Typography level="body-sm" fontWeight="500" component="p" sx={{ mt: 2, width: { md: '380px' } }}>
             Smart Investing Starts with Pag-IBIG: Your Path to Affordable Homeownership
           </Typography>
         </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', mt: 4 }}>
-          <Input placeholder="Search" sx={{ width: '300px', mr: 2 }} />
-          <Button variant="solid">Search</Button>
+        <Box sx={{ display: 'flex', mt: 4, width: { xs: '100%', md: '80%' } }}>
+          <Input
+            placeholder="Search"
+            startDecorator={<Search sx={{ color: '#0B6BCB' }} />}
+            sx={{
+              width: '100%',
+              maxWidth: '300px',
+              height: '45px',
+              borderRadius: '5px',
+              borderColor: '#0B6BCB',
+              backgroundColor: '#E3EFFB',
+              color: '#0B6BCB',
+              fontWeight: 'bold'
+            }}
+          />
         </Box>
       </Box>
-      <Box sx={{ width: { xs: '100%', md: '47.40vw'},height:{xs:'100%', md:'50vh'}, mt: { xs: 4, md: 0 } }}>
+      <Box
+        sx={{
+          display: { xs: 'none', md: 'block' }
+        }}
+      >
         <Landingcarousel />
       </Box>
     </Box>
